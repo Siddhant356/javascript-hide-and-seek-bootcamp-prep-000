7ba2bbd1387d24d49aaf126349df103fb752639f
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-05-01 16:12:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  document.querySelector('#nested .target')
+}
+function increaseRankBy(n){
+ parseInt(document.querySelector('.ranked-list'))+n
+}
+function deepestChild(){
+  
+}
